f1071dc210832106a880e007c95d7ca7ac648711
smilykuy <smilykuy8@gmail.com>
2021-08-17 05:52:55 +0700

    testing random item

--- src/test/java/smily/plugin/summonrandomitem/event/RandomItemTest.java
+++ src/test/java/smily/plugin/summonrandomitem/event/RandomItemTest.java
@@ -1,10 +1,31 @@
 package smily.plugin.summonrandomitem.event;
 
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
 
 class RandomItemTest {
 
-    @Test
-    void spawnItem() {
+
+    Plugin plugin;
+    Player player;
+
+    @BeforeEach
+    void setUp(){
+        player = mock(Player.class);
+        plugin = mock(Plugin.class);
     }
+
+
+    @Test
+    public void isExecuting(){
+
+    }
+
+
 }
+
